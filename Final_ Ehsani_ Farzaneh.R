b0a1a00e1ef94942e1a836eb44589d85abe04365
Farzaneh Ehsani <sehsanioskooie@ucdavis.edu>
2024-12-10 22:13:50 -0800

    Add files via upload

--- Final_ Ehsani_ Farzaneh.R
+++ Final_ Ehsani_ Farzaneh.R
@@ -8,17 +8,18 @@ surveys_2<-surveys %>%
          minutes_per_mile<=10 & 
          minutes_per_mile>5) %>% 
          filter( minutes_per_mile>1) %>%
-  mutate(new_cat = case_when(
-  month%in%1:6 & year==2024~ "first_period",
-  month>6 & year==2024)~"second_period",
-  year!=2024~ "third_period") %>% 
-  mutate(speed = (total_distance_m / total_elapsed_time_s)*60)
+  mutate(time_period = case_when(
+    as.Date(timestamp) < as.Date("2024-01-01") ~ "Pre_2024",
+    as.Date(timestamp) >= as.Date("2024-01-01") & as.Date(timestamp) <= as.Date("2024-06-30") ~ "Rehab_Jan_Jun_2024",
+    as.Date(timestamp) > as.Date("2024-06-30") ~ "Post_July_2024"
+  )) %>% 
+    mutate(speed = (total_distance_m / total_elapsed_time_s) * 60)
   
 surveys_2 %>% 
-  ggplot(aes(x = steps_per_minute, y = speed, color= new_cat)) +
+  ggplot(aes(x = steps_per_minute, y =speed, color= time_period)) +
   geom_boxplot()+
-  geom_smooth(method = "lm", se = FALSE, aes(group = new_cat), 
-              color = "cyan", size = 2, linetype = "solid")+
+  geom_smooth(method = "lm", se = FALSE, aes(group = time_period), 
+              color = "cyan", size = 1, linetype = "solid")+
   labs(
     title = "Boxplot of Speed by Steps per Minute",
     x = "Steps per Minute (SPM)",
